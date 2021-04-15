d43d982bb09a3bf162df2e74224b1365a7fbd360
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-04-15 21:54:27 +0530

    Update terrafromAction.groovy

--- vars/terrafromAction.groovy
+++ vars/terrafromAction.groovy
@@ -6,30 +6,33 @@ def call(Map stepsParams)
         git branch: "${stepsParams.branch}",
         url: "${stepsParams.git_url}"
     }
-    stage('Initializing Terraform Code')
+    dir('Terraform')
     {
-        sh 'terraform init'
-        echo "Hello Y"
-    }
-    stage('Validating Terraform Code')
-    {
-        sh 'terraform validate'
-        echo "Hello YP"
-    }
-    stage('Checking Formatting')
-    {
-        //sh 'terraform fmt'
-        echo "Hello YPS"
-    }
-    stage('Applying Plan')
-    {
-        //sh 'terraform plan'
-        echo "Hello YPSB"
-    }
-    stage('Applying Terraform Code')
-    {
-        //input message: 'Press Yes to apply changes', ok: 'YES'
-        //h 'terraform apply -lock=false -auto-approve'
-        echo "Hello P"
+        stage('Initializing Terraform Code')
+        {
+            sh 'terraform init'
+            echo "Hello Y"
+        }
+        stage('Validating Terraform Code')
+        {
+            sh 'terraform validate'
+            echo "Hello YP"
+        }
+        stage('Checking Formatting')
+        {
+            //sh 'terraform fmt'
+            echo "Hello YPS"
+        }
+        stage('Applying Plan')
+        {
+            //sh 'terraform plan'
+            echo "Hello YPSB"
+        }
+        stage('Applying Terraform Code')
+        {
+            //input message: 'Press Yes to apply changes', ok: 'YES'
+            //h 'terraform apply -lock=false -auto-approve'
+            echo "Hello P"
+        }
     }
 }
