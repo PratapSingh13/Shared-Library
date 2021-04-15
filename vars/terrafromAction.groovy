953d01753a23dadab122851adc3a29463d41fdd8
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-04-15 21:37:48 +0530

    Create terrafromAction.groovy

---
+++ vars/terrafromAction.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+def call(Map stepsParams)
+{
+    stage('Code Checkout')
+    {
+        git branch: "${stepsParams.branch}",
+        url: "${stepsParams.git_url}"
+    }
+    stage('Initializing Terraform Code')
+    {
+        //sh 'terraform init'
+        echo "Hello Y"
+    }
+    stage('Validating Terraform Code')
+    {
+        //sh 'terraform validate'
+        echo "Hello YP"
+    }
+    stage('Checking Formatting')
+    {
+        //sh 'terraform fmt'
+        echo "Hello YPS"
+    }
+    stage('Applying Plan')
+    {
+        //sh 'terraform plan'
+        echo "Hello YPSB"
+    }
+    stage('Applying Terraform Code')
+    {
+        //input message: 'Press Yes to apply changes', ok: 'YES'
+        //h 'terraform apply -lock=false -auto-approve'
+        echo "Hello P"
+    }
+}
