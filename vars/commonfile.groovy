39ccea0e9d434a032c2d32dd55e3a9b770aacd08
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-04-16 15:06:30 +0530

    Update commonfile.groovy

--- vars/commonfile.groovy
+++ vars/commonfile.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 
-def approvalStep() {
-    input message: 'Press Yes to apply changes', ok: 'YES'
-}
-
 def readPropertyFile(Map stepParams) {
     config = readProperties file: "${stepParams.configFilePath}"
     return config
