ae1c8353ac588717b911d72eb7f59b991e806e57
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-13 12:54:22 +0530

    Update terraformAction.groovy

--- vars/terraformAction.groovy
+++ vars/terraformAction.groovy
@@ -1,24 +1,19 @@
 #!/usr/bin env groovy
 def executeAction(Map stepParams) 
 {
-    dir("${stepParams.codePath}") 
+  dir("${stepParams.codePath}") 
+  {  
+    if ("${stepParams.operation}" == "apply")
     {
-        
-        if ("${stepParams.operation}" == "apply")
-        {
-            sh "terraform ${stepParams.operation} -lock=false -auto-approve"
-        }
-        else
-        {
-            sh "terraform ${stepParams.operation}"
-        }
+      sh "terraform ${stepParams.operation} -lock=false -auto-approve"
     }
-}
-
-def executeLinting(Map stepParams)
-{
-    dir("${stepParams.codePath}")
+    else
     {
-        sh "tflint"
+      sh "terraform ${stepParams.operation}"
     }
+    if ("${stepParams.operation}" == "destroy")
+    {
+      sh "terraform ${stepParams.operation} -lock=false -auto-approve"
+    }
+  }
 }
