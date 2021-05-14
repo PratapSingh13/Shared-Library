36d2db9817ae4f4e8cfb29e5ffa9693c40a0f75e
Yogendra Pratap Singh <yogendra.pratap@opstree.com>
2021-05-14 16:05:14 +0530

    line break added

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -5,6 +5,6 @@ def sendGoogleNotification(Map stepParams) {
     def build_num = env.BUILD_NUMBER
     def job_name = env.JOB_NAME
     googlechatnotification url: "https://chat.googleapis.com/v1/spaces/AAAAuvH0DTI/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=6bxkYEGZWPNi1G449fc7FCvyX7PP5v6Xx1rLo3uWLTE%3D",
-    message: "*Job* :  <br>*${env.JOB_NAME}* started by *${user}* user with buildnumber *${env.BUILD_NUMBER}* was ${stepParams.buildStatus} . BUILD_URL:- ${env.BUILD_URL}"   
+    message: "*Job* :  *${env.JOB_NAME}* "<br>started by *${user}* user" with buildnumber *${env.BUILD_NUMBER}* was ${stepParams.buildStatus} . BUILD_URL:- ${env.BUILD_URL}"   
   }
 }
