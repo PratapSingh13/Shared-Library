ee62186dc1ab751084ddc3487de8b3d03f1d040c
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-16 07:30:16 +0530

    Update commonfile.groovy

--- vars/commonfile.groovy
+++ vars/commonfile.groovy
@@ -4,3 +4,15 @@ def readPropertyFile(Map stepParams) {
     config = readProperties file: "${stepParams.configFilePath}"
     return config
 }
+
+def approvalStep() {
+    stage("Waiting for Approval") {
+        input 'Do you want to proceed or not?'
+    }
+}
+
+def cleanWorkspace() {
+    stage("Clean WorkSpace") {
+        cleanWs()
+    }
+}
