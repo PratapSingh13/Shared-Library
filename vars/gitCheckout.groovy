f8463beb4660c2e0cb4acd9cbce15070f30793ad
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-16 07:21:27 +0530

    Delete gitCheckout.groovy

--- vars/gitCheckout.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-def call(Map stageParams) {
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
