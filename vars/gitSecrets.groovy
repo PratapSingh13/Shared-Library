3eb0c433ca1fe4b82ab8c1c65fcdd8fed12d3bb7
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-28 16:20:25 +0530

    Update gitSecrets.groovy

--- vars/gitSecrets.groovy
+++ vars/gitSecrets.groovy
@@ -23,7 +23,7 @@ def provisionReporting() {
                     sh 'pwd'
                     def count = sh(script: 'cat secretkeys.txt | wc -l', returnStdout: true).trim()
                     sh 'cat secretkeys.txt'
-                    def count = sh(script: 'cat secretkeys.txt)
+                    //def count = sh(script: 'cat secretkeys.txt)
                     if(count != "1")
                     sh "cp secretkeys.txt ../secret_scan/${project}.txt"
                }
