9b19ebd66745dcde50a966801e1646c5f1f389ea
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-01 23:06:56 +0530

    Update testSL.groovy

--- vars/testSL.groovy
+++ vars/testSL.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def buildDeployer()
+def buildDeployer(Map stepParams)
 {
     try
     {
@@ -8,6 +8,30 @@ def buildDeployer()
     catch(Exception e)
     {
         echo "Unable to clone the Repository"
+        echo e.toString()
+        throw e
+    }
+    try 
+    {
+        config = commonfile.readPropertyFile(
+            configFilePath: "${stepParams.configFilePath}"
+        )
+    }
+    catch (Exception e) 
+    {
+        echo "Sorry I'm unable to read Config file"
+        echo e.toString()
+        throw e
+    }
+    try
+    {
+        gs.provisionReporting(
+            git_url: "${config.git_url}"
+        )
+    }
+    catch(Exception e)
+    {
+        echo "Unable to Scan Credentials"
         echo e.toString()
         throw e
     }
