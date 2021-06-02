a4da821e0c3572423952c25c5b2ec5a38201e0b4
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-02 17:48:14 +0530

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+def buildDeployer(Map stepParams)
+{
+    try
+    {
+        git.checkoutCode()
+    }
+    catch(Exception e)
+    {
+        echo "Unable to clone the Repository"
+        echo e.toString()
+        throw e
+    }
+    try 
+    {
+        config = commonfile.readPropertyFile(
+            configFilePath: "${stepParams.configFilePath}"
+        )
+    }
+    catch (Exception e) 
+    {
+        echo "Sorry I'm unable to read Config file"
+        echo e.toString()
+        throw e
+    }
+    try
+    {
+         //env = "${config.env}
+      sh 'aws s3 cp s3://da-app-configuration/${config.env}/web/application/gulpfile.js .'
+    }
+    catch (Exception e)
+    {
+         echo "Failed while Code Build"
+         echo e.toString()
+         throw e
+    }
+}
+  
