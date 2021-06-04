7a8c0d3d6893590ea127ac0a32c288f7a91249cd
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-04 10:07:49 +0530

    Update gitSecrets.groovy

--- vars/gitSecrets.groovy
+++ vars/gitSecrets.groovy
@@ -17,7 +17,6 @@ def provisionReporting() {
                sh "rm -rf AWS"
                sh "mkdir AWS"
                dir("AWS") {
-                    //git credentialsId: 'GitHub', url: 'https://github.com/PratapSingh13/Java.git'
                     checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'GitHub', url: "${repo}"]]])
                     sh label: '', script: '{ git secrets --scan; } 2> secretkeys.txt | true'
                     sh 'pwd'
